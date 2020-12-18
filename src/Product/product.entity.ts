884034da4a6ae2e3b74e46edc6de952e6a67e7d1
Shubhangi Palkar <shubhangi.palkar@sts.local>
2020-12-18 20:04:01 +0530

    Files has updated

--- src/Product/product.entity.ts
+++ src/Product/product.entity.ts
@@ -14,4 +14,7 @@ export class ProductDB {
 
   @Column()
   price: number;
+
+  // @Column()
+  // Updatedprice: number;
 }
--- src/Product/product.service.ts
+++ src/Product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable , NotFoundException} from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { title } from "process";
 import { Repository } from "typeorm";
@@ -6,62 +6,62 @@ import { ProductDB } from "./product.entity";
 import { Product } from "./product.model";
 
 @Injectable()
-export class ProductService{
-    constructor (@InjectRepository(ProductDB)
-    private productRepository: Repository<ProductDB>,){}
+export class ProductService {
+    constructor(@InjectRepository(ProductDB)
+    private productRepository: Repository<ProductDB>,) { }
 
-    products : Product[] = [];
-    
-    insertProduct(title : string , desc: string , price: number){
-        const prodId = Math.round(Math.random()*10).toString();
-        const newProduct = new Product (prodId, title, desc, price);
+    products: Product[] = [];
+
+    insertProduct(title: string, desc: string, price: number) {
+        const prodId = Math.round(Math.random() * 10).toString();
+        const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
         //  return title;
         return newProduct;
 
     }
 
-    getProducts(){
+    getProducts() {
         return [...this.products];
     }
 
-    getSingleProduct(prodId: string){
+    getSingleProduct(prodId: string) {
         const product = this.findProduct(prodId)[0];
-        return {...product};
+        return { ...product };
 
     }
 
 
-    getUpdateProduct(prodId: string, prodTitle: string, prodDesc: string, prodPrice: number){
+    getUpdateProduct(prodId: string, prodTitle: string, prodDesc: string, prodPrice: number) {
         const [product, index] = this.findProduct(prodId);
-        const updatedProduct = {...product};
-        if(prodTitle){
+        const updatedProduct = { ...product };
+        if (prodTitle) {
             updatedProduct.title = prodTitle;
-            console.log ("title found");
+            console.log("title found");
         }
-        if(prodDesc){
+        if (prodDesc) {
             updatedProduct.description = prodDesc;
-            console.log ("description found");
+            console.log("description found");
         }
-        if(prodPrice){
+        if (prodPrice) {
             updatedProduct.price = prodPrice;
-            console.log ("price found");
+            console.log("price found");
         }
         this.products[index] = updatedProduct;
-        
+
     }
 
-    DeleteProduct(prodId: string){
+    DeleteProduct(prodId: string) {
 
         const index = this.findProduct(prodId)[1];
-        this.products.splice(index,1);
-     }
+        this.products.splice(index, 1);
+    }
 
-    private findProduct(id : string) : [Product , number] {
+    private findProduct(id: string): [Product, number] {
         const productIndex = this.products.findIndex((prod) => prod.id === id);
         const product = this.products[productIndex];
-        if (!product){
-throw new NotFoundException ('Could not find product');
+        if (!product) {
+            throw new NotFoundException('Could not find product');
         }
         return [product, productIndex];
     }
